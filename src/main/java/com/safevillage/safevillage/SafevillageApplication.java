dae7b439058ea364a44a4f53a356047580d93ac4
chaeyn <chaulfe@gmail.com>
2025-12-13 21:25:55 +0900

    init

---
+++ src/main/java/com/safevillage/safevillage/SafevillageApplication.java
@@ -0,0 +1,13 @@
+package com.safevillage.safevillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafevillageApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SafevillageApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/safevillage/safevillage/SafevillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safevillage.safevillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafevillageApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
